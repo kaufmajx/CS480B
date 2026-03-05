25f32c8596356b619a73e729286f7404f5fbe404
tenley139 <166758447+tenleyk@users.noreply.github.com>
2026-03-04 19:07:12 -0500

    small tweak to close polygons

--- PA3/src/geography/Polygon.java
+++ PA3/src/geography/Polygon.java
@@ -3,21 +3,20 @@ package geography;
 import java.awt.Shape;
 import java.awt.geom.Path2D;
 
-public class Polygon extends PieceWiseLinearCurve
-{
+public class Polygon extends PieceWiseLinearCurve {
 
-  public Polygon(String id)
-  {
-    super(id);
-  }
+	public Polygon(String id) {
+		super(id);
+	}
 
-  public Polygon(String id, Path2D.Double shape)
-  {
-    super(id, shape);
-  }
+	public Polygon(String id, Path2D.Double shape) {
+		super(id, shape);
+	}
 
-  public Shape getShape()
-  {
-    return super.getShape();
-  }
+	public Shape getShape() {
+		// Must close the polygon if necessary before returning
+		Path2D.Double closed = (Path2D.Double) shape.clone();
+		closed.closePath();
+		return closed;
+	}
 }
